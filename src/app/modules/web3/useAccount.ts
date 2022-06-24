b757819a446d9fe29e1a22f1653d28f4e83e2799
saxon <1@1.com>
2022-06-24 21:47:39 +0430

    generic types

--- src/app/modules/web3/useAccount.ts
+++ src/app/modules/web3/useAccount.ts
@@ -2,7 +2,7 @@ import useSWR from 'swr'
 import {CryptoHookFactory} from 'types/hooks'
 
 // deps -> provider, ethereum, contract (web3State)
-export const hookFactory: CryptoHookFactory = (deps) => (params) => {
+export const hookFactory: CryptoHookFactory<string, string> = (deps) => (params) => {
   const swrRes = useSWR('web3/useAccount', () => {
     console.log(deps)
     console.log(params)
