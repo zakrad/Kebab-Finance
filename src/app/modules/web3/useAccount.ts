de6c575b38375adc522fcf513ec43d21e8b56422
saxon <1@1.com>
2022-06-25 21:00:28 +0430

    handle accounts changed

--- src/app/modules/web3/useAccount.ts
+++ src/app/modules/web3/useAccount.ts
@@ -1,3 +1,4 @@
+import {useEffect} from 'react'
 import useSWR from 'swr'
 import {CryptoHookFactory} from 'types/hooks'
 
@@ -27,6 +28,23 @@ export const hookFactory: AccountHookFactory =
         revalidateOnFocus: false,
       }
     )
+
+    useEffect(() => {
+      ethereum?.on('accountsChanged', handleAccountsChanged)
+      return () => {
+        ethereum?.removeListener('accountsChanged', handleAccountsChanged)
+      }
+    })
+
+    const handleAccountsChanged = (...args: unknown[]) => {
+      const accounts = args[0] as string[]
+      if (accounts.length === 0) {
+        console.error('Please, connect to Web3 wallet')
+      } else if (accounts[0] !== swrRes.data) {
+        alert('changed')
+        console.log(accounts[0])
+      }
+    }
 
     const connect = async () => {
       try {
