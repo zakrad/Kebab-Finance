e5dd20b1f724c5b84e700933ddf412323bb9bec3
saxon <1@1.com>
2022-06-26 22:19:51 +0430

    handle wallet lock

--- src/app/providers/web3/index.tsx
+++ src/app/providers/web3/index.tsx
@@ -3,8 +3,25 @@ import {createDefaultState, createWeb3State, Web3State} from './utils'
 import {ethers} from 'ethers'
 import {MetaMaskInpageProvider} from '@metamask/providers'
 
-function pageReload() {
+const pageReload = () => {
   window.location.reload()
+}
+
+const handleAccount = (ethereum: MetaMaskInpageProvider) => async () => {
+  const isLocked = !(await ethereum._metamask.isUnlocked())
+  if (isLocked) {
+    pageReload()
+  }
+}
+
+const setGlobalListeners = (ethereum: MetaMaskInpageProvider) => {
+  ethereum.on('chainChanged', pageReload)
+  ethereum.on('accountsChanged', handleAccount(ethereum))
+}
+
+const removeGlobalListeners = (ethereum: MetaMaskInpageProvider) => {
+  ethereum.removeListener('chainChanged', pageReload)
+  ethereum.removeListener('accountsChanged', handleAccount)
 }
 
 const Web3Context = createContext<Web3State>(createDefaultState())
@@ -37,16 +54,8 @@ const Web3Provider: FunctionComponent = ({children}) => {
       }
     }
     initWeb3()
-    return removeGlobalListeners(window.ethereum)
+    return () => removeGlobalListeners(window.ethereum)
   }, [])
-
-  const setGlobalListeners = (ethereum: MetaMaskInpageProvider) => {
-    ethereum.on('chainChanged', pageReload)
-  }
-
-  const removeGlobalListeners = (ethereum: MetaMaskInpageProvider) => {
-    ethereum.removeListener('chainChanged', pageReload)
-  }
 
   return <Web3Context.Provider value={web3Api}>{children}</Web3Context.Provider>
 }
