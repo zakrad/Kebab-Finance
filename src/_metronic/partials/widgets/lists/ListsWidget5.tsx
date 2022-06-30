fd5585af85457558d1d8f62a6ccd15935d9590f3
saxon <1@1.com>
2022-06-30 20:47:52 +0430

    Complete activities

--- src/_metronic/partials/widgets/lists/ListsWidget5.tsx
+++ src/_metronic/partials/widgets/lists/ListsWidget5.tsx
@@ -24,6 +24,7 @@ const ListsWidget5: React.FC<Props> = ({className}) => {
       gasPrice: {_hex: string; _isBigNumber: boolean}
       hash: string
       nonce: number
+      time: Date
       timestamp: number
       to: string
       transactionIndex: number
@@ -46,14 +47,14 @@ const ListsWidget5: React.FC<Props> = ({className}) => {
           historical.push(tx)
         })
       })
-      historical.forEach((tx) => {
-        tx.time = new Date(tx.timestamp!)
-      })
+      // historical.forEach((tx) => {
+      //   tx.time = new Date(tx.timestamp!)
+      // })
       setTxs(historical.length)
       setLast(historical.slice(-10).reverse())
       // console.log(lastTx)
     }
-    getHistory('0x47fA47B59276765705FC4a3640D4880F98A081fa')
+    getHistory('0x3845badAde8e6dFF049820680d1F14bD3903a5d0')
   }, [])
   // const getHistory = async (address: string) => {
   //   await etherscanProvider.getHistory(address).then((history) => {
@@ -82,33 +83,18 @@ const ListsWidget5: React.FC<Props> = ({className}) => {
       <div className='card-body pt-5'>
         {/* begin::Timeline */}
         <div className='timeline-label'>
-          {/* begin::Item */}
-          <div className='timeline-item'>
-            {/* begin::Label */}
-            <div className='timeline-label fw-bolder text-gray-800 fs-6'>08:42</div>
-            {/* end::Label */}
-            {/* begin::Badge */}
-            <div className='timeline-badge'>
-              <i className='fa fa-genderless text-warning fs-1'></i>
-            </div>
-            {/* end::Badge */}
-            {/* begin::Text */}
-            <div className='fw-mormal timeline-content text-muted ps-3'>
-              Outlines keep you honest. And keep structure
-            </div>
-            {/* end::Text */}
-          </div>
-          {/* end::Item */}
           {last.map((tx, i) => {
             return (
               <>
                 <div className='timeline-item'>
-                  <div className='timeline-label fw-bolder text-gray-800 fs-6'>08:42</div>
+                  <div className='timeline-label fw-bolder text-gray-800 fs-7'>
+                    {new Date(tx.timestamp * 1000).toISOString().substring(5, 10).replace('-', '/')}
+                  </div>
                   <div className='timeline-badge'>
                     <i
                       className={
                         'fa ' +
-                        (tx.to === '0x47fA47B59276765705FC4a3640D4880F98A081fa'
+                        (tx.to === '0x3845badAde8e6dFF049820680d1F14bD3903a5d0'
                           ? 'fa-arrow-circle-down text-success '
                           : 'fa-arrow-circle-up text-danger ') +
                         'fs-1'
@@ -118,7 +104,7 @@ const ListsWidget5: React.FC<Props> = ({className}) => {
                   <div
                     className={
                       'fw-mormal timeline-content ps-3 ' +
-                      (tx.to === '0x47fA47B59276765705FC4a3640D4880F98A081fa'
+                      (tx.to === '0x3845badAde8e6dFF049820680d1F14bD3903a5d0'
                         ? 'fw-bolder text-gray-800'
                         : 'text-muted')
                     }
@@ -129,11 +115,11 @@ const ListsWidget5: React.FC<Props> = ({className}) => {
                       ) * 1e4
                     ) / 1e4}{' '}
                     Ether from{' '}
-                    {tx.from === '0x47fA47B59276765705FC4a3640D4880F98A081fa'
+                    {tx.from === '0x3845badAde8e6dFF049820680d1F14bD3903a5d0'
                       ? 'you'
                       : `"${tx.from.substring(0, 7)}..."`}{' '}
                     to{' '}
-                    {tx.to === '0x47fA47B59276765705FC4a3640D4880F98A081fa'
+                    {tx.to === '0x3845badAde8e6dFF049820680d1F14bD3903a5d0'
                       ? 'you'
                       : `"${tx.to.substring(0, 7)}..."`}
                   </div>
@@ -149,120 +135,6 @@ const ListsWidget5: React.FC<Props> = ({className}) => {
               </>
             )
           })}
-          {/* begin::Item */}
-          <div className='timeline-item'>
-            {/* begin::Label */}
-            <div className='timeline-label fw-bolder text-gray-800 fs-6'>14:37</div>
-            {/* end::Label */}
-            {/* begin::Badge */}
-            <div className='timeline-badge'>
-              <i className={'fa fa-arrow-circle-down fa-arrow-circle-up text-success fs-1'}></i>
-            </div>
-            {/* end::Badge */}
-            {/* begin::Desc */}
-            <div className='timeline-content fw-bolder text-gray-800 ps-3'>
-              Make deposit
-              <a href='#' className='text-primary'>
-                USD 700
-              </a>
-              . to ESL
-            </div>
-            {/* end::Desc */}
-          </div>
-          {/* end::Item */}
-          {/* begin::Item */}
-          <div className='timeline-item'>
-            {/* begin::Label */}
-            <div className='timeline-label fw-bolder text-gray-800 fs-6'>16:50</div>
-            {/* end::Label */}
-            {/* begin::Badge */}
-            <div className='timeline-badge'>
-              <i className='fa fa-genderless text-primary fs-1'></i>
-            </div>
-            {/* end::Badge */}
-            {/* begin::Text */}
-            <div className='timeline-content fw-mormal text-muted ps-3'>
-              Indulging in poorly driving and keep structure keep great
-            </div>
-            {/* end::Text */}
-          </div>
-          {/* end::Item */}
-          {/* begin::Item */}
-          <div className='timeline-item'>
-            {/* begin::Label */}
-            <div className='timeline-label fw-bolder text-gray-800 fs-6'>21:03</div>
-            {/* end::Label */}
-            {/* begin::Badge */}
-            <div className='timeline-badge'>
-              <i className='fa fa-genderless text-danger fs-1'></i>
-            </div>
-            {/* end::Badge */}
-            {/* begin::Desc */}
-            <div className='timeline-content fw-bold text-gray-800 ps-3'>
-              New order placed
-              <a href='#' className='text-primary'>
-                #XF-2356
-              </a>
-              .
-            </div>
-            {/* end::Desc */}
-          </div>
-          {/* end::Item */}
-          {/* begin::Item */}
-          <div className='timeline-item'>
-            {/* begin::Label */}
-            <div className='timeline-label fw-bolder text-gray-800 fs-6'>16:50</div>
-            {/* end::Label */}
-            {/* begin::Badge */}
-            <div className='timeline-badge'>
-              <i className='fa fa-genderless text-primary fs-1'></i>
-            </div>
-            {/* end::Badge */}
-            {/* begin::Text */}
-            <div className='timeline-content fw-mormal text-muted ps-3'>
-              Indulging in poorly driving and keep structure keep great
-            </div>
-            {/* end::Text */}
-          </div>
-          {/* end::Item */}
-          {/* begin::Item */}
-          <div className='timeline-item'>
-            {/* begin::Label */}
-            <div className='timeline-label fw-bolder text-gray-800 fs-6'>21:03</div>
-            {/* end::Label */}
-            {/* begin::Badge */}
-            <div className='timeline-badge'>
-              <i className='fa fa-genderless text-danger fs-1'></i>
-            </div>
-            {/* end::Badge */}
-            {/* begin::Desc */}
-            <div className='timeline-content fw-bold text-gray-800 ps-3'>
-              New order placed
-              <a href='#' className='text-primary'>
-                #XF-2356
-              </a>
-              .
-            </div>
-            {/* end::Desc */}
-          </div>
-          {/* end::Item */}
-          {/* begin::Item */}
-          <div className='timeline-item'>
-            {/* begin::Label */}
-            <div className='timeline-label fw-bolder text-gray-800 fs-6'>10:30</div>
-            {/* end::Label */}
-            {/* begin::Badge */}
-            <div className='timeline-badge'>
-              <i className='fa fa-genderless text-success fs-1'></i>
-            </div>
-            {/* end::Badge */}
-            {/* begin::Text */}
-            <div className='timeline-content fw-mormal text-muted ps-3'>
-              Finance KPI Mobile app launch preparion meeting
-            </div>
-            {/* end::Text */}
-          </div>
-          {/* end::Item */}
         </div>
         {/* end::Timeline */}
       </div>
