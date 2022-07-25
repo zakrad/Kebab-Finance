9d329e93755938091307c9ac4319b09c220f02bc
saxon <1@1.com>
2022-07-25 23:27:01 +0430

    update

--- src/app/modules/compound/components/SupplyWithdraw.js
+++ src/app/modules/compound/components/SupplyWithdraw.js
@@ -3,7 +3,7 @@ import { ethers } from "ethers";
 import { parseEther } from "ethers/lib/utils";
 import TokensAbi from 'src/app/modules/compound/abi/TokensAbi.json'
 
-
+let testAddress = '0x859e9d8a4edadfEDb5A2fF311243af80F85A91b8'
 
 const Supply = async (account, cTokenAddress, ticker, amount, cToken) => {
     const underlyingDecimals = Compound.decimals[cToken.slice(1, 10)];
@@ -25,11 +25,8 @@ const Supply = async (account, cTokenAddress, ticker, amount, cToken) => {
         data: "0x1249c58b",
         value: parseEther(`${amount}`)
     });
-    const estimateGasApprove = await provider.estimateGas({
-        to: addressT,
-        data: "0x095ea7b3",
-        value: ("0xa2c67EaC1Cc3DD40441C9f631fb53D3c5BA2eC41", parseEther(`${amount}`))
-    });
+    const estimateGasApprove = await tokenContract.estimateGas.approve(account.data, parseEther(`${amount}`));
+
     const feeData = await provider.getFeeData()
     console.log(feeData, estimateGasSupply, estimateGasApprove)
     // let tx = await tokenContract.mint({
